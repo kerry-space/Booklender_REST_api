ea5b0da4617a316b9b30fcefde61815fc442da68
kerry jalal <khariy34@hotmail.com>
2023-02-09 17:44:51 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/kerry/booklender_rest_api/BooklenderRestApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.kerry.booklender_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklenderRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklenderRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/kerry/booklender_rest_api/BooklenderRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.kerry.booklender_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
